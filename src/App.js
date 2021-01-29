12fd9012132464aa0cf9ff03ee9ed322dd0f89ea
AritraDutta <aritradutta120@gmail.com>
2021-01-29 11:53:03 +0530

    Assests added + Header Done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
 import './App.css';
+import Header from './components/Header.js';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Raj!</h1>
+      <Header/>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Header.css';
+import TeslaLogo from '../assets/teslaLogoSmall.svg';
+
+const Header = () =>{
+    return(
+        <div className="header">
+            <div className="header__logo">
+                <img src={TeslaLogo} alt="Tesla Logo"/>
+            </div>
+
+            <div className="header__center">
+                <p>Modeel S</p>
+                <p>Modeel 3</p>
+                <p>Modeel X</p>
+                <p>Modeel Y</p>
+                <p>Solar Roof</p>
+                <p>Solar Pannel</p>
+            </div>
+
+            <div className="header__right">
+                <p>Shop</p>
+                <p>Tesla Account</p>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
